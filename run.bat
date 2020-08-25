7537fbaee18ae85d23ac3c84896aee8f7fe8bd2f
DerekLoveCC <zhaomingbo1010@126.com>
2020-08-25 21:25:53 +0800

    add bat to run app

---
+++ run.bat
@@ -0,0 +1,5 @@
+set COR_ENABLE_PROFILING=1
+set COR_PROFILER_PATH=D:\Derek\GitHub\JustShim\JustShimProfiler\Debug\JustShimProfiler.dll
+set COR_PROFILER={03F2B361-A89D-463A-AA65-DB711D4448A0}
+
+.\bin\x86\Debug\CSharpTest.exe
